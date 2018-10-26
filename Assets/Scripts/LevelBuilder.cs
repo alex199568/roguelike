6a4e10f508417f69c57b2c41cc3d122a93ff5e47
Alexander Bondarenco <sasha.199568@gmail.com>
2018-10-27 00:01:54 +0100

    Refactor level building

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -12,33 +12,103 @@ public class LevelBuilder : MonoBehaviour
 
     void Start()
     {
-        float cellWidth = floor.GetComponent<Renderer>().bounds.size.x;
-        float cellHeight = floor.GetComponent<Renderer>().bounds.size.y;
+        Level level = GenerateLevel();
+        BuildLevel(level);
+    }
 
-        float xOffset = width * cellWidth / 2.0f;
-        float zOffset = height * cellHeight / 2.0f;
+    private void BuildLevel(Level level)
+    {
+        if (level.IsEmpty)
+        {
+            return;
+        }
 
+        Vector2 cellSize = level.CellSize;
+
+        float xOffset = width * cellSize.x / 2.0f;
+        float zOffset = height * cellSize.y / 2.0f;
+
+        foreach (KeyValuePair<Vector2Int, Cell> cell in level)
+        {
+            GameObject newCell = cell.Value.CellObject;
+            Vector3 newPosition = new Vector3(
+                transform.position.x + cell.Key.x * cellSize.x - xOffset,
+                0.0f,
+                transform.position.y + cell.Key.y * cellSize.y - zOffset
+            );
+            Instantiate(newCell, newPosition, transform.rotation);
+        }
+    }
+
+    private Level GenerateLevel()
+    {
+        Level result = new Level();
         for (int i = 0; i < width; ++i)
         {
             for (int j = 0; j < height; ++j)
             {
-                GameObject newCell;
-                if (Random.value > 0.5f)
+                Cell cell;
+                if (Random.value > 0.5)
                 {
-                    newCell = floor;
+                    cell = new Cell(floor);
                 }
                 else
                 {
-                    newCell = wall;
+                    cell = new Cell(wall);
                 }
-
-                Vector3 newPosition = new Vector3(
-                    transform.position.x + i * cellWidth - xOffset,
-                    0.0f,
-                    transform.position.y + j * cellHeight - zOffset
-                    );
-                Instantiate(newCell, newPosition, transform.rotation);
+                result.AddCell(new Vector2Int(i, j), cell);
             }
+        }
+        return result;
+    }
+
+    class Level : IEnumerable
+    {
+        private Dictionary<Vector2Int, Cell> cells = new Dictionary<Vector2Int, Cell>();
+        private Vector2 cellSize;
+
+        public Vector2 CellSize
+        {
+            get { return cellSize; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return cells.Count == 0; }
+        }
+
+        public void AddCell(Vector2Int position, Cell cell)
+        {
+            cells.Add(position, cell);
+
+            // TODO: optimize
+            Vector3 size = cell.CellObject.GetComponent<Renderer>().bounds.size;
+            cellSize = new Vector2(size.x, size.z);
+        }
+
+        public Cell GetCellAt(Vector2Int position)
+        {
+            return cells[position];
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return cells.GetEnumerator();
+        }
+    }
+
+    class Cell
+    {
+        private GameObject cellObject;
+
+        public GameObject CellObject
+        {
+            get { return cellObject; }
+        }
+
+        public Cell(GameObject cellObject)
+        {
+            this.cellObject = cellObject;
         }
     }
 }
