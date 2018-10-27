274565b2b0ef965e53a81b9ac3d09eee9e280945
Alexander Bondarenco <sasha.199568@gmail.com>
2018-10-27 23:23:31 +0100

    Create grid array

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -11,8 +11,15 @@ public class LevelBuilder : MonoBehaviour
     public int width;
     public int height;
 
+    private float cellWidth;
+    private float cellHeight;
+
     void Start()
     {
+        Vector3 size = floor.GetComponent<Renderer>().bounds.size;
+        cellWidth = size.x;
+        cellHeight = size.z;
+
         Level level = GenerateLevel();
         BuildLevel(level);
     }
@@ -24,63 +31,65 @@ public class LevelBuilder : MonoBehaviour
             return;
         }
 
-        Vector2 cellSize = level.CellSize;
-
-        foreach (KeyValuePair<Vector2Int, Cell> cell in level)
+        for (int i = 0; i < level.Width; ++i)
         {
-            GameObject newCell = cell.Value.CellObject;
-            Vector3 newPosition = new Vector3(
-                transform.position.x + cell.Key.x * cellSize.x,
-                0.0f,
-                transform.position.y + cell.Key.y * cellSize.y
-            );
-            Instantiate(newCell, newPosition, transform.rotation);
+            for (int j = 0; j < level.Height; ++j)
+            {
+                Cell cell = level.GetCellAt(i, j);
+                if (cell != null)
+                {
+                    GameObject newCell = cell.CellObject;
+                    Vector3 newPosition = new Vector3(
+                    transform.position.x + i * cellWidth,
+                    0.0f,
+                    transform.position.y + j * cellHeight);
+                    Instantiate(newCell, newPosition, transform.rotation);
+                }
+            }
         }
     }
 
     private Level GenerateLevel()
     {
         Level result = new Level();
-        PlaceRoom(result, new RectInt(1, 1, 4, 4));
-        PlaceRoom(result, new RectInt(-5, -5, 2, 2));
-        PlaceRoom(result, new RectInt(5, -3, 10, 5));
+        PlaceRoom(result, new RectInt(0, 0, 24, 24));
         return result;
     }
 
     private void PlaceRoom(Level level, RectInt position)
     {
-        Cell floorCell = new Cell(floor);
-        Cell wallCell = new Cell(wall);
+        Cell floorCell = new Cell(floor, CellType.Floor);
+        Cell wallCell = new Cell(wall, CellType.Wall);
 
         for (int i = position.xMin + 1; i < position.xMax; ++i)
         {
-            level.AddCell(new Vector2Int(i, position.yMin), wallCell);
-            level.AddCell(new Vector2Int(i, position.yMax), wallCell);
+            level.AddCell(i, position.yMin, wallCell);
+            level.AddCell(i, position.yMax, wallCell);
         }
 
         for (int i = position.yMin + 1; i < position.yMax; ++i)
         {
-            level.AddCell(new Vector2Int(position.xMin, i), wallCell);
-            level.AddCell(new Vector2Int(position.xMax, i), wallCell);
+            level.AddCell(position.xMin, i, wallCell);
+            level.AddCell(position.xMax, i, wallCell);
         }
 
-        level.AddCell(new Vector2Int(position.xMin, position.yMin), wallCell);
-        level.AddCell(new Vector2Int(position.xMin, position.yMax), wallCell);
-        level.AddCell(new Vector2Int(position.xMax, position.yMin), wallCell);
-        level.AddCell(new Vector2Int(position.xMax, position.yMax), wallCell);
+        level.AddCell(position.xMin, position.yMin, wallCell);
+        level.AddCell(position.xMin, position.yMax, wallCell);
+        level.AddCell(position.xMax, position.yMin, wallCell);
+        level.AddCell(position.xMax, position.yMax, wallCell);
 
         for (int i = position.xMin + 1; i < position.xMax; ++i)
         {
             for (int j = position.yMin + 1; j < position.yMax; ++j)
             {
-                level.AddCell(new Vector2Int(i, j), floorCell);
+                level.AddCell(i, j, floorCell);
             }
         }
     }
 
-    class Level : IEnumerable
+    class Level
     {
-        private Dictionary<Vector2Int, Cell> cells = new Dictionary<Vector2Int, Cell>();
+        private ArrayGrid<Cell> cellsGrid = new ArrayGrid<Cell>();
         private Vector2 cellSize;
 
         public Vector2 CellSize
@@ -90,48 +99,61 @@ public class LevelBuilder : MonoBehaviour
 
         public bool IsEmpty
         {
-            get { return cells.Count == 0; }
+            get { return cellsGrid.IsEmpty; }
         }
 
-        public void AddCell(Vector2Int position, Cell cell)
+        public int Width
+        {
+            get { return cellsGrid.Width; }
+        }
+
+        public int Height
+        {
+            get { return cellsGrid.Height; }
+        }
+
+        public void AddCell(int x, int y, Cell cell)
         {
             try
             {
-                cells.Add(position, cell);
+                cellsGrid.Set(x, y, cell);
             }
-            catch (ArgumentException)
+            catch (IndexOutOfRangeException)
             {
-                Debug.LogWarningFormat("Could not add cell at {0}", position);
+                Debug.LogWarningFormat("could not add cell at {0}, {1}", x, y);
             }
-
-            // TODO: optimize
-            Vector3 size = cell.CellObject.GetComponent<Renderer>().bounds.size;
-            cellSize = new Vector2(size.x, size.z);
         }
 
-        public Cell GetCellAt(Vector2Int position)
+        public Cell GetCellAt(int x, int y)
         {
-            return cells[position];
-        }
-
-        public IEnumerator GetEnumerator()
-        {
-            return cells.GetEnumerator();
+            return cellsGrid.Get(x, y);
         }
     }
 
     class Cell
     {
         private GameObject cellObject;
+        private CellType cellType;
+
+        public CellType Type
+        {
+            get { return cellType; }
+        }
 
         public GameObject CellObject
         {
             get { return cellObject; }
         }
 
-        public Cell(GameObject cellObject)
+        public Cell(GameObject cellObject, CellType cellType)
         {
+            this.cellType = cellType;
             this.cellObject = cellObject;
         }
+    }
+
+    enum CellType
+    {
+        Floor, Wall
     }
 }
