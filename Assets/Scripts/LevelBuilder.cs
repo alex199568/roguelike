23cec817a9921baba79964ecc867080202869d77
Alexander Bondarenco <sasha.199568@gmail.com>
2018-10-28 12:19:06 +0000

    Passages (#1)
    * Create grid array
    
    * Build passages
    
    * Code style fix

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 
 public class LevelBuilder : MonoBehaviour
 {
-
     public GameObject floor;
-    public GameObject wall;
     public int width;
     public int height;
 
+    private float cellWidth;
+    private float cellHeight;
+
     void Start()
     {
+        Vector3 size = floor.GetComponent<Renderer>().bounds.size;
+        cellWidth = size.x;
+        cellHeight = size.z;
+
         Level level = GenerateLevel();
         BuildLevel(level);
     }
@@ -24,99 +29,136 @@ public class LevelBuilder : MonoBehaviour
             return;
         }
 
-        Vector2 cellSize = level.CellSize;
-
-        foreach (KeyValuePair<Vector2Int, Cell> cell in level)
+        for (int i = 0; i < level.Width; ++i)
         {
-            GameObject newCell = cell.Value.CellObject;
-            Vector3 newPosition = new Vector3(
-                transform.position.x + cell.Key.x * cellSize.x,
-                0.0f,
-                transform.position.y + cell.Key.y * cellSize.y
-            );
-            Instantiate(newCell, newPosition, transform.rotation);
+            for (int j = 0; j < level.Height; ++j)
+            {
+                Cell cell = level.GetCellAt(i, j);
+                if (cell != null)
+                {
+                    GameObject newCell = cell.CellObject;
+                    Vector3 newPosition = new Vector3(
+                    transform.position.x + i * cellWidth,
+                    0.0f,
+                    transform.position.y + j * cellHeight);
+                    Instantiate(newCell, newPosition, transform.rotation);
+                }
+            }
         }
     }
 
     private Level GenerateLevel()
     {
         Level result = new Level();
-        PlaceRoom(result, new RectInt(1, 1, 4, 4));
-        PlaceRoom(result, new RectInt(-5, -5, 2, 2));
-        PlaceRoom(result, new RectInt(5, -3, 10, 5));
+        RectInt room1Position = new RectInt(0, 0, 6, 4);
+        RectInt room2Position = new RectInt(12, 8, 7, 10);
+        RectInt room3Position = new RectInt(1, 12, 5, 3);
+        RectInt room4Position = new RectInt(3, 10, 8, 15);
+        PlaceRoom(result, room1Position);
+        PlaceRoom(result, room2Position);
+        PlaceRoom(result, room3Position);
+        PlaceRoom(result, room4Position);
+        ConnectWithPassage(result, room1Position, room2Position);
+        ConnectWithPassage(result, room2Position, room3Position);
+        ConnectWithPassage(result, room3Position, room1Position);
         return result;
+    }
+
+    private void ConnectWithPassage(Level level, RectInt room1, RectInt room2)
+    {
+        System.Random random = new System.Random();
+        Cell floorCell = new Cell(floor);
+
+        int startX = random.Next(room1.xMin + 1, room1.xMax - 1);
+        int startY = random.Next(room1.yMin + 1, room1.yMax - 1);
+        int endX = random.Next(room2.xMin + 1, room2.xMax - 1);
+        int endY = random.Next(room2.yMin + 1, room2.yMax - 1);
+
+        int stepX = startX < endX ? 1 : -1;
+        int stepY = startY < endY ? 1 : -1;
+
+        int i = startX;
+        int j = startY;
+        while (true)
+        {
+            bool xChanged = false;
+            if (Math.Abs(startX - i) < Math.Abs(startX - endX))
+            {
+                i += stepX;
+                xChanged = true;
+            }
+
+            bool yChanged = false;
+            if (Math.Abs(startY - j) < Math.Abs(startY - endY))
+            {
+                j += stepY;
+                yChanged = true;
+            }
+
+            if (xChanged && yChanged)
+            {
+                level.AddCell(i, j, floorCell);
+                level.AddCell(i - stepX, j, floorCell);
+                level.AddCell(i, j - stepY, floorCell);
+            }
+            else if (xChanged || yChanged)
+            {
+                level.AddCell(i, j, floorCell);
+            }
+            else
+            {
+                break;
+            }
+        }
     }
 
     private void PlaceRoom(Level level, RectInt position)
     {
         Cell floorCell = new Cell(floor);
-        Cell wallCell = new Cell(wall);
-
-        for (int i = position.xMin + 1; i < position.xMax; ++i)
-        {
-            level.AddCell(new Vector2Int(i, position.yMin), wallCell);
-            level.AddCell(new Vector2Int(i, position.yMax), wallCell);
-        }
-
-        for (int i = position.yMin + 1; i < position.yMax; ++i)
-        {
-            level.AddCell(new Vector2Int(position.xMin, i), wallCell);
-            level.AddCell(new Vector2Int(position.xMax, i), wallCell);
-        }
-
-        level.AddCell(new Vector2Int(position.xMin, position.yMin), wallCell);
-        level.AddCell(new Vector2Int(position.xMin, position.yMax), wallCell);
-        level.AddCell(new Vector2Int(position.xMax, position.yMin), wallCell);
-        level.AddCell(new Vector2Int(position.xMax, position.yMax), wallCell);
 
         for (int i = position.xMin + 1; i < position.xMax; ++i)
         {
             for (int j = position.yMin + 1; j < position.yMax; ++j)
             {
-                level.AddCell(new Vector2Int(i, j), floorCell);
+                level.AddCell(i, j, floorCell);
             }
         }
     }
 
-    class Level : IEnumerable
+    class Level
     {
-        private Dictionary<Vector2Int, Cell> cells = new Dictionary<Vector2Int, Cell>();
-        private Vector2 cellSize;
-
-        public Vector2 CellSize
-        {
-            get { return cellSize; }
-        }
+        private ArrayGrid<Cell> cellsGrid = new ArrayGrid<Cell>();
 
         public bool IsEmpty
         {
-            get { return cells.Count == 0; }
+            get { return cellsGrid.IsEmpty; }
         }
 
-        public void AddCell(Vector2Int position, Cell cell)
+        public int Width
+        {
+            get { return cellsGrid.Width; }
+        }
+
+        public int Height
+        {
+            get { return cellsGrid.Height; }
+        }
+
+        public void AddCell(int x, int y, Cell cell)
         {
             try
             {
-                cells.Add(position, cell);
+                cellsGrid.Set(x, y, cell);
             }
-            catch (ArgumentException)
+            catch (IndexOutOfRangeException)
             {
-                Debug.LogWarningFormat("Could not add cell at {0}", position);
+                Debug.LogWarning($"could not add cell at {x}, {y}");
             }
-
-            // TODO: optimize
-            Vector3 size = cell.CellObject.GetComponent<Renderer>().bounds.size;
-            cellSize = new Vector2(size.x, size.z);
         }
 
-        public Cell GetCellAt(Vector2Int position)
+        public Cell GetCellAt(int x, int y)
         {
-            return cells[position];
-        }
-
-        public IEnumerator GetEnumerator()
-        {
-            return cells.GetEnumerator();
+            return cellsGrid.Get(x, y);
         }
     }
 
