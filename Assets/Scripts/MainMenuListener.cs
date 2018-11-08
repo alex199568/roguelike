53798a2c4e14035a7d4a65b5e967a62391c281c6
Alexander Bondarenco <sasha.199568@gmail.com>
2018-11-08 19:33:47 +0000

    Create main menu

---
+++ Assets/Scripts/MainMenuListener.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuListener : MonoBehaviour
+{
+    public Button StartButton;
+    public Button QuitButton;
+    
+    void Start()
+    {
+        StartButton.onClick.AddListener(StartGame);
+        QuitButton.onClick.AddListener(Quit);
+    }
+    
+    void StartGame()
+    {
+#if UNITY_EDITOR
+        Debug.Log("Starting a new game");
+#else
+        SceneManager.LoadScene("Game");
+#endif
+    }
+
+    void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    void Start()
+    {
+        if (!Application.isEditor)
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = false;
+        }
+    }
+}
